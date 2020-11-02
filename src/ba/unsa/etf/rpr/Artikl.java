8380be3b009f0dc9b2898297cac0650ea2ba66ef
jhasanovic <jhasanovic1@etf.unsa.ba>
2020-11-02 02:36:50 +0100

    Atributi klase Artikl proglašeni za private

--- src/ba/unsa/etf/rpr/Artikl.java
+++ src/ba/unsa/etf/rpr/Artikl.java
@@ -1,9 +1,9 @@
 package ba.unsa.etf.rpr;
 
 public class Artikl {
-    String naziv;
-    int cijena;
-    String kod;
+    private String naziv;
+    private int cijena;
+    private String kod;
     public Artikl(String naziv,int cijena,String kod) {
         this.naziv = naziv;
         this.cijena = cijena;
